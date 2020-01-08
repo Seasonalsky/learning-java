1de770c3217fa4d5c2db796b47a77b6911399855
Luna Ong <jeweliaisme@gmail.com>
2020-01-08 12:01:08 +0800

    Create images and button

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.androidfundamentals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //R stands for resources, case sensitive
+        // id means looking for a certain id, in this case button3) in resources
+        Button myJavaButton = findViewById(R.id.button3);
+        myJavaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Remember to alt+enter when prompt comes up to import relevant stuff
+                Toast.makeText(getApplicationContext(),
+                        "yeet", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
